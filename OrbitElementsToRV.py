c7e7223015be22f4db11f030e1d8d0a2bbb9e74c
Loceyi <45290120+loceyi@users.noreply.github.com>
2018-12-10 16:35:21 +0800

    2018/12/10

--- OrbitElementsToRV.py
+++ OrbitElementsToRV.py
@@ -1,16 +1,63 @@
+#
 #Base on ICRS of IERS,
 #input:a,e,i,Omega, omega,E output:
-#角度单位为弧度
-from numpy import *
-def orbitelementtorv (a,e,i,Omega,omega,f):
 
-    P=mat([[cos(Omega)*cos(omega)-sin(Omega)*sin(omega)*cos(i)],
-    [sin(Omega)*cos(omega)+cos(Omega)*sin(omega)*cos(i)],[sin(omega)*sin(i)]])
-    Q=mat([[-cos(Omega)*sin(omega)-sin(Omega)*cos(omega)*cos(i)], [sin(Omega)*sin(omega)+cos(Omega)*cos(omega)*cos(i)],
-        [cos(omega)*sin(i)]])
-    d=a*(1-e*e)/(1+e*cos(f));
-    r=d*cos(f)*P+d*sin(f)*Q;
+from numpy import mat,cos,sin,array
+from math import radians
+# import numpy as np
+#
+# class MyOrbit:
+#     def __init__(self, a):
+#         self.local_var_init(a)
+#         self.var_a = a
+#
+#     def local_var_init(self, a):
+#         if 2<a<5:
+#             self.var_a = a
+
+
+def orbit_element_to_rv (orbit_element):
+    '''
+    :param orbit_element: semi_major_axis, Eccentricity,
+           Inclination, RAAN, Perigee, True_Anomaly
+           输入时要求半长轴单位为km,其余角度单位为度
+    :return: r,velocity
+
+    '''
+    semi_major_axis=orbit_element[0]
+    Eccentricity=orbit_element[1]
+    Inclination=radians(orbit_element[2])
+    RAAN=radians(orbit_element[3])
+    Perigee=radians(orbit_element[4])
+    True_Anomaly=radians(orbit_element[5])
+    P_row_1=[cos(RAAN) * cos(Perigee) - sin(RAAN) * sin(Perigee) * cos(Inclination)]
+    P_row_2=[sin(RAAN) * cos(Perigee) + cos(RAAN) * sin(Perigee) * cos(Inclination)]
+    P_row_3=[sin(Perigee) * sin(Inclination)]
+    P=mat([P_row_1, P_row_2, P_row_3])
+    Q_row_1=[-cos(RAAN) * sin(Perigee) - sin(RAAN) * cos(Perigee) * cos(Inclination)]
+    Q_row_2=[sin(RAAN) * sin(Perigee) + cos(RAAN) * cos(Perigee) * cos(Inclination)]
+    Q_row_3=[cos(Perigee) * sin(Inclination)]
+    Q=mat([ Q_row_1,Q_row_2 ,Q_row_3 ])
+    d= semi_major_axis * (1 - Eccentricity * Eccentricity) / (1 + Eccentricity * cos(True_Anomaly))
+    r= d * cos(True_Anomaly) * P + d * sin(True_Anomaly) * Q
 
     return r
-r=orbitelementtorv(7000,0,0,0,0,0)
-print(r)
+
+def test():
+    a=array([7000,0,0,0,0,360],dtype=float)
+    b=orbit_element_to_rv(a)
+    print(b)
+
+
+
+if __name__ == "__main__":
+    # r = orbitelementtorv(7000, 0, 0, 0, 0, 0)
+    # print(r)
+    # l1 = [1,2,3]
+    # l2 = list(range(3))
+    # l3 = list('456')
+    # mtx1 = np.array([l1, l2, l3], dtype=int32);
+    # print(mtx1, type(mtx1))
+    # print(mtx1 * 3)
+    # print(mtx1[1,1:2] * 3 )
+    test()
---
+++ Perturbation.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy.integrate import odeint
+from math import sqrt,cos,pi
+from math import radians
+
+
+
+def d_earth_nonsphericfigure(orbit_element,t):
+    '''
+    :param Orbit_Elements: 轨道六根数,单位为弧度
+    :param t: 时间
+    :return: 微分方程右边的表达式，地球非球型摄动
+    '''
+    R_earth=6378 #km
+    mu=398600 #km^3/s^(-2)
+    J2=-1.08264*10**(-3)
+    semi_major_axis = orbit_element[0]
+    Eccentricity = orbit_element[1]
+    Inclination = orbit_element[2]
+    RAAN = orbit_element[3]
+    Perigee = orbit_element[4]
+    True_Anomaly = orbit_element[5]
+    p=semi_major_axis**(1-Eccentricity**2)
+    n=sqrt(mu/(semi_major_axis)**3)
+    d_RAAN=(-2/3)*(R_earth/p)**2*n*J2*cos(Inclination)
+    d_Perigee=-3/4*(R_earth/p)**2*n*J2*(1-5*(cos(Inclination)**2))
+    return np.array([0, 0, 0,d_RAAN,d_Perigee,n])
+
+def d_lunar_solar(orbit_element,t):
+    '''
+    :param Orbit_Elements: 轨道六根数,单位为弧度
+    :param t: 时间
+    :return: 微分方程右边的表达式，日月引力摄动
+    '''
+    R_earth=6378 #km
+    mu=398600 #km^3/s^(-2)
+    J2=-1.08264*10**(-3)
+    semi_major_axis = orbit_element[0]
+    Eccentricity = orbit_element[1]
+    Inclination = orbit_element[2]
+    RAAN = orbit_element[3]
+    Perigee = orbit_element[4]
+    True_Anomaly = orbit_element[5]
+    p=semi_major_axis**(1-Eccentricity**2)
+    n=sqrt(mu/(semi_major_axis)**3)
+    d_RAAN=(-2/3)*(R_earth/p)**2*n*J2*cos(Inclination)
+    d_Perigee=-3/4*(R_earth/p)**2*n*J2*(1-5*(cos(Inclination)**2))
+    return np.array([0, 0, 0,d_RAAN,d_Perigee,n])
+
+
+
+t = np.arange(0, 36000, 0.01)
+P1 = odeint(d_earth_nonsphericfigure, (7000,0,0,0,0,0),t)  # (0.,1.,0.)是point的初值
+
+def test():
+    import pylab as pl
+    P1[:, 5] = P1[:, 5] % (2 * pi)
+    pl.plot(t, P1[:, 5])
+    pl.show()
+
+
+
+if __name__ == "__main__":
+
+    test()
+
+
+
+
+
+from skyfield.api import load
+
+planets = load('de421.bsp')
+earth, mars = planets['earth'], planets['mars']
+
+ts = load.timescale()
+t = ts.now()
+astrometric = earth.at(t).observe(mars)
+ra, dec, distance = astrometric.radec()
+
+print(ra)
+print(dec)
+print(distance)
